e347d5a3d0d5ac85163183ffa49e5c1811f31aaa
TangLian <96833954+LiangT721@users.noreply.github.com>
2022-09-15 14:44:48 -0600

    add setHeader of res

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accpet, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.post('/api/projects', projects.createProject);
 
 app.get('/api/projects', projects.getProjects);
